e311adc02fa8543820a60ecadb06e8fb73c739e7
vidya <vidya.anantharam@thomsonreuters.com>
2021-04-28 17:39:13 +0530

    First Commit

---
+++ HelloWorldSB/src/main/java/com/infotech/app/HelloWorldSPApp.java
@@ -0,0 +1,13 @@
+package com.infotech.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSPApp {
+	
+		public static void main(String[] args) {
+			SpringApplication.run(HelloWorldSPApp.class, args);
+
+}
+}
